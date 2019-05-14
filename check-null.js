4a13e721e22084d5e0b5bbd307091291dbe7666c
mhlee80 <myeonghyeon.lee@gmail.com>
2019-05-14 10:43:20 +0900

    널체크

--- check-null.js
+++ check-null.js
@@ -3,9 +3,27 @@ function checkNull(a) {
   console.log("is undefefined?:", a === undefined);
   console.log("is null?:", a === null);  
   console.log("is not null?:", !a);
+  
+  if (a) {
+    console.log("a is not null");
+  } else {
+    console.log("a is null");
+  }
+
+  if (!!a) {
+    console.log("a is not null");
+  } else {
+    console.log("a is null");
+  }
 }
 
-checkNull();
+// checkNull();
 // checkNull(null);
 // checkNull("");
 // checkNull({});
+// checkNull([]);
+
+let dict = {};
+console.log(dict.not_declared_variable == null);
+
+console.log(dict.not_declared_variable != null);
